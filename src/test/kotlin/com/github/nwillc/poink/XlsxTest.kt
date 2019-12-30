163888fa216228d77c666405357ec599232bb2bc
Nicholas Christopher <nwillc@gmail.com>
2019-12-29 23:12:46 -0500

    linting

--- src/test/kotlin/com/github/nwillc/poink/XlsxTest.kt
+++ src/test/kotlin/com/github/nwillc/poink/XlsxTest.kt
@@ -2,7 +2,6 @@ package com.github.nwillc.poink
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.io.FileOutputStream
 
 class XlsxTest {
     @Test
